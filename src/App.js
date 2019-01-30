9e3b0303c2cb09faa7dc07db09311610e17b22a5
zudevon <devonrasch@gmail.com>
2019-01-30 16:40:50 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
+/* eslint-disable */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {connect} from 'react-redux';
+import {sendMessage} from "./chat";
+
 
 class App extends Component {
   render() {
+    const {feed, sendMessage} = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          <h1>Hello Bot!</h1>
+          <ul>
+              {feed.map(entry => <li>{entry.text}</li>)}
+          </ul>
+          <input type="text" onKeyDown={(e) => e.keyCode === 13 ? sendMessage(e.target.value) : null}/>
+        </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  feed: state
+
+});
+
+export default connect(mapStateToProps, {sendMessage})(App);
---
+++ src/chat.js
@@ -0,0 +1,47 @@
+import {ApiAiClient} from "api-ai-javascript";
+import {applyMiddleware, createStore} from "redux";
+
+
+const accessToken = '352088e786114e9c9f9004eaf8a590e1';
+const client = new ApiAiClient({accessToken})
+
+
+const ON_MESSAGE = 'ON_MESSAGE';
+
+export const sendMessage = (text, sender='user') => ({
+    type: ON_MESSAGE,
+    payload: {text, sender}
+});
+
+const messageMiddleware = () => next => action => {
+
+    next(action);
+
+    function onSuccess(response) {
+            const {result: {fulfillment}} = response;
+            next(sendMessage(fulfillment.speech, 'bot'));
+        }
+
+    if(action.type === ON_MESSAGE) {
+        const {text} = action.payload;
+        client.textRequest(text)
+            .then( onSuccess )
+
+    }
+};
+
+const initState = [{text: 'hey'}];
+
+const messageReducer = (state = initState, action) => {
+    switch (action.type) {
+
+        case ON_MESSAGE:
+            return [...state, action.payload];
+
+        default:
+            return state;
+
+    }
+};
+
+export const store = createStore(messageReducer, applyMiddleware(messageMiddleware));
