934ad1848860531d008f7f306f680fb34ffc7b59
zudevon <devon.rasch@degreeanalytics.com>
2019-02-04 14:57:35 -0600

    added handleKeyDown function

--- src/App.js
+++ src/App.js
@@ -5,27 +5,35 @@ import {sendMessage} from "./chat";
 
 
 class App extends Component {
-    // state = {
-    //     text_field : ''
-    // };
-    //
-    // handle_edit = e => {
-    //     this.setState({text_field : e.target.value})
-    // };
-    //
-    // onSubmit = e => {
-    //     this.sendMessage(e.target.value);
-    //     this.setState({text_field : ""})
-    // };
+
+    constructor(props) {
+        super(props)
+        this.state = {textField: ''}
+    }
+
+    _enterText = e => this.setState({textField: e.target.value})
+
+    _handleKeyDown = e => {
+        if(e.keyCode === 13) {
+                         this.props.sendMessage(e.target.value)
+            this.setState({textField: ''})
+
+        }
+    }
+
 
   render() {
     const {feed, sendMessage} = this.props;
+
     return (
         <div>
           <ul>
               {feed.map(entry => <li>{entry.text}</li>)}
           </ul>
-          <input type="text" onKeyDown={(e) => e.keyCode === 13 ? sendMessage(e.target.value) : null}/>
+          <input type="text" onChange={this._enterText}
+                 value={this.state.textField}
+                 onKeyDown={this._handleKeyDown}
+          />
         </div>
     );
   }
